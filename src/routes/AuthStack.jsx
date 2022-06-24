1a2a9c5e48cfe07b651c07c447edcc0fda502ca4
Shahriar Ratul <ratul794@gmail.com>
2022-06-24 23:36:55 +0600

    auth screen

--- src/routes/AuthStack.jsx
+++ src/routes/AuthStack.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import PhoneScreen from '../screens/PhoneScreen';
+import OtpScreen from '../screens/OtpScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +19,22 @@ const AuthStack = () => {
             fontWeight: 'bold',
           }, }} />
       <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Register Here', headerStyle: {
+            backgroundColor: '#f3b344',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }, }} />
+      <Stack.Screen name="Otp" component={OtpScreen} options={{ title: 'OTP', headerStyle: {
+            backgroundColor: '#f3b344',
+          },
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }, }} />
+      <Stack.Screen name="Phone" component={PhoneScreen} options={{ title: 'OTP', headerStyle: {
             backgroundColor: '#f3b344',
           },
           headerTintColor: '#fff',
