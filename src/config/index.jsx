dde252bc7873f8515b053d27754b0c464213fa66
Shahriar Ratul <ratul794@gmail.com>
2022-07-25 22:11:34 +0600

    update

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,3 +1,2 @@
 export const BASE_URL = 'https://api.10xtrucks.com';
 export const GOOGLE_MAP_API_KEY = 'AIzaSyBvmhx8TzRvMn2knRFl8HzcSSKDhxyffOI';
-// export const GOOGLE_MAP_API_KEY = 'AIzaSyA32d7EcW3UhgEZ5vgA7o2HBkAEw0UEaN0';
